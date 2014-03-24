ec374b8d535aff5c9ac173b9c537aebd4cd63d74
C457 <android.c357@gmail.com>
2014-03-24 11:51:09 -0500

    golden:Call codinalte overlay in device tree
    Golden has the same keylayout and overlay as codinalte

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+DEVICE_PACKAGE_OVERLAYS += device/samsung/codinalte/overlay
+
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/golden/golden-vendor.mk)
 
