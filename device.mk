3d7e1d8c52cc82a9f2a3c49c3720c8f3ca5700ce
Ozzy2403 <oskar.kratochvil@gmx.de>
2015-07-20 22:00:39 +0200

    bring in sync with novas cm tree
    Change-Id: Ic5f74b1fb9a36a5943fa8ce351aff39793bfa398

--- device.mk
+++ device.mk
@@ -1,7 +1,5 @@
 #
-# Copyright (C) 2013 The Android Open Source Project
-# Copyright (C) 2013 Óliver García Albertos (oliverarafo@gmail.com)
-# Copyright (C) 2014 SlimRoms Project
+# Copyright (C) 2014 NovaFusion http://novafusion.pl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,128 +14,64 @@
 # limitations under the License.
 #
 
-# AOSP specific overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/aosp
+# Add overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/golden/overlay
 
 # Define kind of DPI
-PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.samsunggolden.rc:root/init.samsunggolden.rc \
-    $(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.samsunggolden.rc:root/ueventd.samsunggolden.rc \
-    $(LOCAL_PATH)/rootdir/init.samsunggolden.usb.rc:root/init.samsunggolden.usb.rc \
-    $(LOCAL_PATH)/rootdir/fstab.samsunggolden:root/fstab.samsunggolden \
-    $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc
+# Hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
 
-# Recovery ramdisk, libraries and modules.
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/rootdir/init.recovery.samsunggolden.rc:root/init.recovery.samsunggolden.rc \
-    $(LOCAL_PATH)/recovery/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
-
-# Inputs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
-
-# Force disable softkeys lights and BLN
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.d/66led_OFF:system/etc/init.d/66led_OFF
-
-# Graphics
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/lib/egl/egl.cfg:system/lib/egl/egl.cfg
-	
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.use_dithering=2 \
-    persist.sys.strictmode.disable=1
-
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/configs/omxloaders:system/omxloaders
-
-# Wifi
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
-
+# Wi-Fi packages
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     dhcpcd.conf \
     wpa_supplicant \
-    libnetcmdiface
+    wpa_supplicant.conf
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0
-    wifi.supplicant_scan_interval=15
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
-
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-# RIL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/ste_modem.sh:system/etc/ste_modem.sh \
-    $(LOCAL_PATH)/configs/etc/cspsa.conf:system/etc/cspsa.conf \
-    $(LOCAL_PATH)/configs/etc/AT/manuf_id.cfg:system/etc/AT/manuf_id.cfg \
-    $(LOCAL_PATH)/configs/etc/AT/model_id.cfg:system/etc/AT/model_id.cfg \
-    $(LOCAL_PATH)/configs/etc/AT/system_id.cfg:system/etc/AT/system_id.cfg
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.multiple=false \
-    ro.telephony.ril_class=SamsungU8500RIL \
-    ro.telephony.sends_barcount=1 \
-    ro.telephony.default_network=0
-
-# GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/sirfgps.conf:system/etc/sirfgps.conf \
-    $(LOCAL_PATH)/configs/etc/gps.conf:system/etc/gps.conf
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/etc/asound.conf:system/etc/asound.conf
-
+# Open-source HALs
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
-    audio.usb.default
+    lights.montblanc
 
-# Power
+# Misc packages
 PRODUCT_PACKAGES += \
-    power.montblanc
+    make_ext4fs \
+    setup_fs \
+    Stk
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zygote.disable_gl_preload=true
-
-# USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb \
-    persist.service.adb.enable=1 \
-    ste.special_fast_dormancy=false
-
-# Charger
+#F2FS
 PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
+    genfstab
+
+# Prebuilt kernel
+PRODUCT_COPY_FILES += \
+    device/samsung/golden/ramdisk/zImage:kernel
+
+# Initial ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/golden/ramdisk/init.samsunggolden.rc:root/init.samsunggolden.rc \
+    device/samsung/golden/ramdisk/init.samsunggolden.usb.rc:root/init.samsunggolden.usb.rc \
+    device/samsung/golden/ramdisk/ueventd.samsunggolden.rc:root/ueventd.samsunggolden.rc \
+    device/samsung/golden/ramdisk/lpm.rc:root/lpm.rc \
+    device/samsung/golden/ramdisk/sbin/charger:root/sbin/charger \
+    device/samsung/golden/ramdisk/res/images/charger/battery_0.png:root/res/images/charger/battery_0.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_1.png:root/res/images/charger/battery_1.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_2.png:root/res/images/charger/battery_2.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_3.png:root/res/images/charger/battery_3.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_4.png:root/res/images/charger/battery_4.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_5.png:root/res/images/charger/battery_5.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_charge.png:root/res/images/charger/battery_charge.png \
+    device/samsung/golden/ramdisk/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
@@ -145,30 +79,27 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    make_ext4fs \
-    setup_fs
+# Codec config files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
 
-# Misc packages
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+# Every single binary blob
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/samsung/golden/prebuilt,system)
 
-# We have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
+# Wi-Fi config
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
-# Use the Dalvik VM specific for devices with 1024 MB of RAM
+# Dalvik config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-# Use U8500 opensource parts
-$(call inherit-product, hardware/samsung/u8500.mk)
-
-# Use the non-open-source parts, if they´re present
-$(call inherit-product, vendor/samsung/golden/golden-vendor.mk)
